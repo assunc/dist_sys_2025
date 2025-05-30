8a4df6fbcbc95af3d41c85040d5f1f5770213842
rohan191102 <br.o.h.a.n.1.5.1.3@gmail.com>
2025-05-30 17:33:26 +0200

    Update BrokerController.java for logout

--- src/main/java/com/example/springsoap/Controller/BrokerController.java
+++ src/main/java/com/example/springsoap/Controller/BrokerController.java
@@ -1,5 +1,6 @@
 package com.example.springsoap.Controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,40 +14,69 @@ import java.util.List;
 public class BrokerController {
 
     @GetMapping("/")
-    public String home(@AuthenticationPrincipal OidcUser user,Model model) {
-        if (user != null) {
-            // You can log the user if needed
-            System.out.println("Logged in as: " + user.getFullName());
+    public String home(@AuthenticationPrincipal OidcUser user, Model model) {
+        boolean isLoggedIn = true;
+        if (user == null)
+        {
+            isLoggedIn = false;
         }
-
+        model.addAttribute("isLoggedIn", isLoggedIn);
         model.addAttribute("title", "Home");
         model.addAttribute("contentTemplate", "index");
         return "layout";
     }
 
+
+
+
+
     @GetMapping("/flights")
-    public String flights(Model model) {
+    public String flights(@AuthenticationPrincipal OidcUser user, Model model) {
+        boolean isLoggedIn = true;
+        if (user == null)
+        {
+            isLoggedIn = false;
+        }
         model.addAttribute("title", "Flights");
         model.addAttribute("flights", List.of("BRU → NYC", "BRU → TOKYO"));
+        model.addAttribute("isLoggedIn", isLoggedIn);
         model.addAttribute("contentTemplate", "flights");
         return "layout";
     }
 
     @GetMapping("/hotels")
-    public String hotels(Model model) {
+    public String hotels(@AuthenticationPrincipal OidcUser user, Model model) {
+        boolean isLoggedIn = true;
+        if (user == null)
+        {
+            isLoggedIn = false;
+        }
+        model.addAttribute("isLoggedIn", isLoggedIn);
         model.addAttribute("title", "Hotels");
         model.addAttribute("contentTemplate", "hotels");
         return "layout";
     }
 
     @GetMapping("/combo")
-    public String combo(Model model) {
+    public String combo(@AuthenticationPrincipal OidcUser user, Model model) {
+        boolean isLoggedIn = true;
+        if (user == null)
+        {
+            isLoggedIn = false;
+        }
+        model.addAttribute("isLoggedIn", isLoggedIn);
         model.addAttribute("title", "Hotels + Flights");
         model.addAttribute("contentTemplate", "combo");
         return "layout";
     }
    @GetMapping("/logged-out")
-public String loggedOut(Model model) {
+public String loggedOut(@AuthenticationPrincipal OidcUser user , Model model) {
+       boolean isLoggedIn = true;
+       if (user == null)
+       {
+           isLoggedIn = false;
+       }
+       model.addAttribute("isLoggedIn", isLoggedIn);
     model.addAttribute("title", "Logged Out");
     model.addAttribute("contentTemplate", "loggedout");
     return "layout";
